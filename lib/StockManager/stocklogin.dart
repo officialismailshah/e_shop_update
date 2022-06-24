8c9359185f363ce6e04b4d342194e18f5a4e0bf0
falakshahzadi <98768215+falakshahzadi@users.noreply.github.com>
2022-06-25 01:46:28 +0500

    changestock

---
+++ lib/StockManager/stocklogin.dart
@@ -0,0 +1,196 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:e_shop/StockManager/uploadItems.dart';
+import 'package:e_shop/Authentication/authenication.dart';
+import 'package:e_shop/Widgets/customTextField.dart';
+import 'package:e_shop/DialogBox/errorDialog.dart';
+import 'package:flutter/material.dart';
+
+import '../Admin/uploadItems.dart';
+
+class AdminSignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              colors: [Colors.redAccent, Colors.blueAccent],
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: Text(
+          "MH GARMENTS",
+          style: TextStyle(
+              fontSize: 55.0, color: Colors.white, fontFamily: "Signatra"),
+        ),
+        centerTitle: true,
+      ),
+      body: AdminSignInScreen(),
+    );
+  }
+}
+
+class AdminSignInScreen extends StatefulWidget {
+  @override
+  _AdminSignInScreenState createState() => _AdminSignInScreenState();
+}
+
+class _AdminSignInScreenState extends State<AdminSignInScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _adminIDTextEditingController =
+      TextEditingController();
+  final TextEditingController _passwordTextEditingController =
+      TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    // _screenHeight = MediaQuery.of(context).size.height;
+
+    return SingleChildScrollView(
+      child: Container(
+        decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+            colors: [Colors.redAccent, Colors.blueAccent],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Image.asset(
+                "images/admin.png",
+                height: 240.0,
+                width: 240.0,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Admin",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    controller: _adminIDTextEditingController,
+                    data: Icons.person,
+                    hintText: "id",
+                    isObsecure: false,
+                  ),
+                  CustomTextField(
+                    controller: _passwordTextEditingController,
+                    data: Icons.person,
+                    hintText: "Password",
+                    isObsecure: true,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            // ignore: deprecated_member_use
+            RaisedButton(
+              onPressed: () {
+                _adminIDTextEditingController.text.isNotEmpty &&
+                        _passwordTextEditingController.text.isNotEmpty
+                    ? loginAdmin()
+                    : showDialog(
+                        context: context,
+                        builder: (c) {
+                          return ErrorAlertDialog(
+                            message: "Please write email and password.",
+                          );
+                        });
+              },
+              color: Colors.red,
+              child: Text(
+                "Login",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              height: 4.0,
+              width: _screenWidth * 0.8,
+              color: Colors.red,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            // ignore: deprecated_member_use
+            FlatButton.icon(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AuthenticScreen())),
+              icon: (Icon(
+                Icons.nature_people,
+                color: Colors.red,
+              )),
+              label: Text(
+                "i'm not Admin",
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  loginAdmin() {
+    FirebaseFirestore.instance.collection("admins").get().then((snapshot) {
+      snapshot.docs.forEach((result) {
+        if (result["id"] != _adminIDTextEditingController.text.trim()) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text("Your id is not correct.")),
+          );
+        } else if (result["password"] !=
+            _passwordTextEditingController.text.trim()) {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(content: Text("Your password is not correct.")),
+          );
+        } else {
+          // ignore: deprecated_member_use
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Welcome dear Admin." + result["name"]),
+            ),
+          );
+
+          setState(() {
+            _adminIDTextEditingController.text = "";
+            _passwordTextEditingController.text = "";
+          });
+
+          Route route = MaterialPageRoute(builder: (c) => UploadPage());
+          Navigator.pushReplacement(context, route);
+        }
+      });
+    });
+  }
+}
