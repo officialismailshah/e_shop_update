9a266019c5ef04438648374518a240d65bafa2dc
falakshahzadi <98768215+falakshahzadi@users.noreply.github.com>
2022-06-28 01:14:29 +0500

    admintabbars

---
+++ lib/Admin/adminidorder.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class AdminIdorder extends StatefulWidget {
+  AdminIdorder({Key key}) : super(key: key);
+
+  @override
+  State<AdminIdorder> createState() => _AdminIdorderkState();
+}
+
+class _AdminIdorderkState extends State<AdminIdorder> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
